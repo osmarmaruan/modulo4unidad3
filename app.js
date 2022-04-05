cae22a24c0bdbeb83f51c8e57a87962a161eafd8
osmar <osmarmaruan@gmail.com>
2022-04-04 23:05:26 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+
+
+app.get('/ejemplo1', function (req, res) {
+  res.send('hola soy un ejemplo 1')
+
+})
+
+
+
+app.get('/ejemplo2', function (req, res) {
+  res.end('hola soy un ejemplo 2')
+
+})
+
+
+
+// app.route('/ejemplo2')
+//   .get(function (req, res) {
+//     res.send('consigue un libro al azar')
+//   })
+
+
+
+app.get('/ejemplo3', function (req, res) {
+  res.json('hola soy un ejemplo 3')
+
+})
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
